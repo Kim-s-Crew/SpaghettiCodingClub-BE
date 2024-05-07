32523b720a15a3a24770e5f780d9da6667416d2c
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-07 11:58:25 +0900

    add : User Entity 설정

--- src/main/java/wercsmik/spaghetticodingclub/domain/user/entity/User.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/user/entity/User.java
@@ -1,4 +1,57 @@
 package wercsmik.spaghetticodingclub.domain.user.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
 public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String refereeEmail;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    @Column
+    private String track;
+
+    public User (String username, String password, String email, String track, String refereeEmail, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.track = track;
+        this.refereeEmail = refereeEmail;
+        this.role = role;
+    }
 }
