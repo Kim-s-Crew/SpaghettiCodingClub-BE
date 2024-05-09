32f4712d02d30ab141e9d6868289b363ef9239aa
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-09 21:15:48 +0900

    add : Jwt 초기설정

--- src/main/java/wercsmik/spaghetticodingclub/global/jwt/JwtAuthorizationFilter.java
+++ src/main/java/wercsmik/spaghetticodingclub/global/jwt/JwtAuthorizationFilter.java
@@ -1,4 +1,65 @@
 package wercsmik.spaghetticodingclub.global.jwt;
 
-public class JwtAuthorizationFilter {
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.filter.OncePerRequestFilter;
+import wercsmik.spaghetticodingclub.global.common.CommonResponse;
+import wercsmik.spaghetticodingclub.global.security.UserDetailsServiceImpl;
+
+@Slf4j(topic = "JWT 검증 및 인가")
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    private final UserDetailsServiceImpl userDetailsServiceImpl;
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+            FilterChain filterChain) throws ServletException, IOException {
+
+        String accessToken = jwtUtil.resolveAccessToken(request);
+
+        if (accessToken != null && jwtUtil.validateToken(accessToken)) {
+            Claims info = jwtUtil.getUserInfoFromToken(accessToken);
+
+            // 인증정보에 유저정보 넣기
+            String username = info.getSubject();
+            SecurityContext context = SecurityContextHolder.createEmptyContext();
+
+            UserDetails userDetails = userDetailsServiceImpl.loadUserByUsername(username);
+
+            Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails,
+                    null, userDetails.getAuthorities());
+
+            context.setAuthentication(authentication);
+
+            SecurityContextHolder.setContext(context);
+
+        } else if (accessToken != null) { // accessToken이 null이 아니지만 유효하지 않은 토큰일 경우
+            CommonResponse commonResponse = new CommonResponse(
+                    "토큰이 유효하지 않습니다.", HttpStatus.FORBIDDEN.value());
+            response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+            response.setContentType("application/json; charset=UTF-8");
+            response.getWriter().write(objectMapper.writeValueAsString(commonResponse));
+            return;
+        }
+        // accessToken이 null이거나 인증 처리가 완료됐다면, 다음 필터로 요청과 응답을 넘김
+        filterChain.doFilter(request, response);
+    }
 }
--- src/main/java/wercsmik/spaghetticodingclub/global/jwt/JwtUtil.java
+++ src/main/java/wercsmik/spaghetticodingclub/global/jwt/JwtUtil.java
@@ -1,4 +1,100 @@
 package wercsmik.spaghetticodingclub.global.jwt;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import wercsmik.spaghetticodingclub.domain.user.entity.UserRoleEnum;
+
+@Component
+@Slf4j
 public class JwtUtil {
+
+    public static final String AUTHORIZATION_HEADER = "Authorization";
+
+    public static final String AUTHORIZATION_KEY = "auth";
+
+    public static final String BEARER_PREFIX = "Bearer ";
+
+    public static final long ACCESS_TOKEN_TIME = 60 * 60 * 1000L; // 60분
+
+    public static final long REFRESH_TOKEN_TIME = 60 * 60 * 24 * 30 * 1000L; // 30일
+
+    private static final Logger logger = LoggerFactory.getLogger("JWT 로그");
+
+    private final SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+
+    @Value("${JWT_SECRET_KEY}")
+    private String secretKey;
+    private Key key;
+
+    @PostConstruct
+    public void init() {
+        byte[] bytes = Base64.getDecoder().decode(secretKey);
+        key = Keys.hmacShaKeyFor(bytes);
+    }
+
+    public String resolveAccessToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(AUTHORIZATION_HEADER);
+        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(BEARER_PREFIX)) {
+            return bearerToken.substring(7);
+        }
+        return null;
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+            return true;
+        } catch (SecurityException | MalformedJwtException e) {
+            log.error("Invalid JWT signature, 유효하지 않는 JWT 서명 입니다.");
+        } catch (ExpiredJwtException e) {
+            log.error("Expired JWT token, 만료된 JWT token 입니다.");
+        } catch (UnsupportedJwtException e) {
+            log.error("Unsupported JWT token, 지원되지 않는 JWT 토큰 입니다.");
+        } catch (IllegalArgumentException e) {
+            log.error("JWT claims is empty, 잘못된 JWT 토큰 입니다.");
+        }
+        return false;
+    }
+
+    public Claims getUserInfoFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
+    }
+
+    public String createAccessToken(String email, UserRoleEnum role) {
+        Date date = new Date();
+
+        return BEARER_PREFIX +
+                Jwts.builder()
+                        .setSubject(email)
+                        .claim(AUTHORIZATION_KEY, role)
+                        .setExpiration(new Date(date.getTime() + ACCESS_TOKEN_TIME))
+                        .setIssuedAt(date)
+                        .signWith(key, signatureAlgorithm)
+                        .compact();
+    }
+
+
+    public void addJwtToHeader(String accessToken,
+            HttpServletResponse response) {
+
+        response.addHeader(AUTHORIZATION_HEADER, accessToken);
+    }
 }
