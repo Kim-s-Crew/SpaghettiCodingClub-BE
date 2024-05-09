71c99dea38c41391898f24a30730ae433a93a26d
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-09 23:55:50 +0900

    feature : Auth 회원가입 API / Controller 구현

--- src/main/java/wercsmik/spaghetticodingclub/domain/auth/controller/AuthController.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/auth/controller/AuthController.java
@@ -1,4 +1,36 @@
 package wercsmik.spaghetticodingclub.domain.auth.controller;
 
+import jakarta.mail.MessagingException;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wercsmik.spaghetticodingclub.domain.auth.dto.EmailCheckRequestDTO;
+import wercsmik.spaghetticodingclub.domain.auth.dto.SignRequestDTO;
+import wercsmik.spaghetticodingclub.domain.auth.service.AuthService;
+import wercsmik.spaghetticodingclub.global.common.CommonResponse;
+
+@RestController
+@RequestMapping("/api/auths")
 public class AuthController {
+
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<CommonResponse<Void>> Signup(
+            @Valid @RequestBody SignRequestDTO signRequestDTO) {
+
+        authService.Signup(signRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponse.of("회원가입 성공", null));
+    }
 }
