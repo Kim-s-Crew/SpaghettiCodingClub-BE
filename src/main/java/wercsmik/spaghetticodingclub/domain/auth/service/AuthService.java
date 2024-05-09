8aabeb752a6bc085307326f6fb030c2a4823ad8e
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-09 23:56:05 +0900

    feature : Auth 회원가입 API / Service 구현

--- src/main/java/wercsmik/spaghetticodingclub/domain/auth/service/AuthService.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/auth/service/AuthService.java
@@ -1,4 +1,51 @@
 package wercsmik.spaghetticodingclub.domain.auth.service;
 
+import java.util.Objects;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import wercsmik.spaghetticodingclub.domain.auth.dto.SignRequestDTO;
+import wercsmik.spaghetticodingclub.domain.user.entity.User;
+import wercsmik.spaghetticodingclub.domain.user.entity.UserRoleEnum;
+import wercsmik.spaghetticodingclub.domain.user.repository.UserRepository;
+import wercsmik.spaghetticodingclub.global.exception.CustomException;
+import wercsmik.spaghetticodingclub.global.exception.ErrorCode;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void Signup(SignRequestDTO signRequestDTO) {
+
+        String username = signRequestDTO.getUsername();
+        String email = signRequestDTO.getEmail();
+        String password = signRequestDTO.getPassword();
+        String checkPassword = signRequestDTO.getCheckPassword();
+        String track = signRequestDTO.getTrack();
+        String refereeEmail = signRequestDTO.getRefereeEmail();
+
+        // 이메일 중복확인
+        if (userRepository.findByEmail(signRequestDTO.getEmail()).isPresent()) {
+            throw new CustomException(ErrorCode.EMAIL_ALREADY_EXIST_);
+        }
+
+        // 비밀번호 != 비밀번호 확인
+        if (!Objects.equals(password, checkPassword)) {
+            throw new CustomException(ErrorCode.PASSWORD_CONFIRMATION_NOT_MATCH);
+        }
+
+        String encodePassword = passwordEncoder.encode(password);
+
+        UserRoleEnum role = UserRoleEnum.USER; // 기본적으로 USER로 설정
+        // refereeEmail 값이 null이 아니고 빈 문자열이 아닐 경우에만 ADMIN 설정
+        if(refereeEmail != null && !refereeEmail.trim().isEmpty()) {
+            role = UserRoleEnum.ADMIN;
+        }
+
+        User user = new User(username, encodePassword, email, track, refereeEmail, role);
+        userRepository.save(user);
+    }
 }
