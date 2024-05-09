e589503fb0ad92afcbe19ab5c2673065b116998c
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-09 21:20:00 +0900

    add : UserRepository 초기설정

--- src/main/java/wercsmik/spaghetticodingclub/domain/user/repository/UserRepository.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/user/repository/UserRepository.java
@@ -1,4 +1,12 @@
 package wercsmik.spaghetticodingclub.domain.user.repository;
 
-public interface UserRepository {
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import wercsmik.spaghetticodingclub.domain.user.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
 }
