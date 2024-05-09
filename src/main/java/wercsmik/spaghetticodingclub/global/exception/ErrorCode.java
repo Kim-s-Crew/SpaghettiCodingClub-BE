61cef9574ed03b09f98bb0f1d0ddabee1200665b
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-09 21:21:54 +0900

    add : ErrorCode 초기설정

--- src/main/java/wercsmik/spaghetticodingclub/global/exception/ErrorCode.java
+++ src/main/java/wercsmik/spaghetticodingclub/global/exception/ErrorCode.java
@@ -8,7 +8,23 @@ public enum ErrorCode {
     // Assesment
 
     // Auth
-    PASSWORD_NOT_MATCH(400, "비밀번호가 일치하지 않습니다.");
+    PASSWORD_NOT_MATCH(401, "비밀번호가 일치하지 않습니다."),
+
+    USERNAME_ALREADY_EXIST(401, "중복된 유저네임입니다."),
+
+    KAKAOID_ALREADY_EXIST(401, "중복된 카카오ID 입니다."),
+
+    EMAIL_ALREADY_EXIST_(401, "중복된 이메일입니다."),
+
+    AUTHENTICATION_NOT_HAVE(401, "권한이 없습니다."),
+
+    PASSWORD_CONFIRMATION_NOT_MATCH(401, "비밀번호와 비밀번호 확인이 일치하지 않습니다."),
+
+    NOT_FOUND_USER(400, "해당 유저가 존재하지 않습니다"),
+
+    EMAIL_VERIFICATION_CODE_INVALID(400, "입력하신 이메일 인증 코드가 유효하지 않습니다"),
+
+    KAKAO_USER_EMAIL_MODIFICATION_EXCEPTION(400, "카카오 로그인한 유저는 이메일을 수정할 수 없습니다."),
 
     // Scheduler
 
@@ -18,7 +34,23 @@ public enum ErrorCode {
 
     // Unlike
 
+    //Jwt
+    INVALID_JWT_SIGNATURE(401, "유효하지 않는 JWT 서명 입니다."),
+
+    EXPIRED_JWT_TOKEN(401, "만료된 JWT token 입니다."),
+
+    UNSUPPORTED_JWT_TOKEN(401, "지원되지 않는 JWT 토큰 입니다."),
+
+    INVALID_JWT_TOKEN(401, "잘못된 JWT 토큰 입니다."),
+
+    REFRESH_TOKEN_NOT_FOUND(404, "Refresh Token을 찾을 수 없습니다."),
+
+    EXPIRED_REFRESH_TOKEN(401, "만료된 Refresh Token 입니다."),
+
+    REVOKED_REFRESH_TOKEN(401, "사용이 중지된 Refresh Token 입니다.");
+
     // User
+
 
     // Common
 
