62e3abbbbd30765b96d06caf59f3cddcb52936e8
김은채 <ke808762@gmail.com>
2024-05-13 14:37:49 +0900

    jar 파일 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 PROJECT_ROOT="/home/ubuntu/app"
-JAR_FILE="$PROJECT_ROOT/build/libs/SpaghettiCodingClub-0.0.1-SNAPSHOT.jar"
+JAR_FILE="$PROJECT_ROOT/kims-spaghetti.jar"
 
 APP_LOG="$PROJECT_ROOT/application.log"
 ERROR_LOG="$PROJECT_ROOT/error.log"
