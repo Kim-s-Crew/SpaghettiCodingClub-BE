0ac7f530961a0e79e1283da3acb12c47d40d4593
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-13 17:47:27 +0900

    fix : 에러메시지에 내부로그로 사용자 이메일 적용

--- src/main/java/wercsmik/spaghetticodingclub/global/security/UserDetailsServiceImpl.java
+++ src/main/java/wercsmik/spaghetticodingclub/global/security/UserDetailsServiceImpl.java
@@ -1,6 +1,8 @@
 package wercsmik.spaghetticodingclub.global.security;
 
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -13,12 +15,17 @@ import wercsmik.spaghetticodingclub.domain.user.repository.UserRepository;
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private static final Logger logger = LoggerFactory.getLogger(UserDetailsServiceImpl.class);
+
 
     @Override
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-        User user = userRepository.findByEmail(email).orElseThrow(() ->
-                 new UsernameNotFoundException("존재하지 않는 사용자입니다.")
-        );
+        User user = userRepository.findByEmail(email).orElseThrow(() -> {
+            logger.error("존재하지 않은 사용자 : {}", email);
+            return new UsernameNotFoundException("존재하지 않는 사용자입니다.");
+        });
+
+        logger.info("사용자 존재 확인: {}", email);
 
         return new UserDetailsImpl(user);
     }
