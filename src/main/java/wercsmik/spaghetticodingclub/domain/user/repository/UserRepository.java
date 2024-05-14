16aa20710675ae8fa8ea824bee789c6ca72b79a1
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-14 16:28:02 +0900

    refactor : 불필요 코드 정리

--- src/main/java/wercsmik/spaghetticodingclub/domain/user/repository/UserRepository.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/user/repository/UserRepository.java
@@ -6,7 +6,5 @@ import wercsmik.spaghetticodingclub.domain.user.entity.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<User> findByUsername(String username);
-
     Optional<User> findByEmail(String email);
 }
