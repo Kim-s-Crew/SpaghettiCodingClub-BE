9eb391e5a7ed0b98af387dc7bb65013da1c34ecb
김은채 <ke808762@gmail.com>
2024-05-17 00:46:24 +0900

    fix: sh 설정 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,6 +13,12 @@ export PROD_DB_USER=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/user"
 export PROD_DB_PASSWORD=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/password" --with-decryption --query "Parameter.Value" --output text)
 export JWT_SECRET_KEY=$(aws ssm get-parameter --name "/prod/spaghetti-app/jwt/secret" --with-decryption --query "Parameter.Value" --output text)
 
+# 환경 변수 출력하여 확인
+echo "PROD_DB_URL=$PROD_DB_URL"
+echo "PROD_DB_USER=$PROD_DB_USER"
+echo "PROD_DB_PASSWORD=$PROD_DB_PASSWORD"
+echo "JWT_SECRET_KEY=$JWT_SECRET_KEY"
+
 TIME_NOW=$(date +%c)
 
 # build 파일 복사
@@ -21,7 +27,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup env PROD_DB_URL=$PROD_DB_URL PROD_DB_USER=$PROD_DB_USER PROD_DB_PASSWORD=$PROD_DB_PASSWORD JWT_SECRET_KEY=$JWT_SECRET_KEY java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 # 현재 실행중인 프로세스 ID 출력
 CURRENT_PID=$(pgrep -f $JAR_FILE)
