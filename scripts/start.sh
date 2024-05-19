758911a31e0fcfe4d70f5cffe4d57d6244ee8ee4
김은채 <ke808762@gmail.com>
2024-05-19 20:36:58 +0900

    fix: sh 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,10 +8,17 @@ ERROR_LOG="$PROJECT_ROOT/error.log"
 DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 # AWS CLI를 사용하여 AWS Systems Manager Parameter Store에서 파라미터 값을 불러옵니다
-export PROD_DB_URL=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/url" --with-decryption --query "Parameter.Value" --output text)
-export PROD_DB_USER=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/user" --with-decryption --query "Parameter.Value" --output text)
-export PROD_DB_PASSWORD=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/password" --with-decryption --query "Parameter.Value" --output text)
-export JWT_SECRET_KEY=$(aws ssm get-parameter --name "/prod/spaghetti-app/jwt/secret" --with-decryption --query "Parameter.Value" --output text)
+PROD_DB_URL=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/url" --with-decryption --query "Parameter.Value" --output text)
+export PROD_DB_URL
+
+PROD_DB_USER=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/user" --with-decryption --query "Parameter.Value" --output text)
+export PROD_DB_USER
+
+PROD_DB_PASSWORD=$(aws ssm get-parameter --name "/prod/spaghetti-app/db/password" --with-decryption --query "Parameter.Value" --output text)
+export PROD_DB_PASSWORD
+
+JWT_SECRET_KEY=$(aws ssm get-parameter --name "/prod/spaghetti-app/jwt/secret" --with-decryption --query "Parameter.Value" --output text)
+export JWT_SECRET_KEY
 
 # 환경 변수 설정 확인
 {
@@ -24,10 +31,10 @@ export JWT_SECRET_KEY=$(aws ssm get-parameter --name "/prod/spaghetti-app/jwt/se
 TIME_NOW=$(date +%c)
 
 # 기존 애플리케이션 종료
-CURRENT_PID=$(pgrep -f $JAR_FILE)
+CURRENT_PID=$(pgrep -f "$JAR_FILE")
 if [ -n "$CURRENT_PID" ]; then
     echo "$TIME_NOW > 기존 애플리케이션 종료: $CURRENT_PID" >> $DEPLOY_LOG
-    kill -15 $CURRENT_PID
+    kill -15 "$CURRENT_PID"
     sleep 5
 fi
 
@@ -38,7 +45,7 @@ if [ -z "$BUILD_JAR" ]; then
     exit 1
 fi
 echo "$TIME_NOW > $BUILD_JAR 파일 복사" >> $DEPLOY_LOG
-cp $BUILD_JAR $JAR_FILE
+cp "$BUILD_JAR" "$JAR_FILE"
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
