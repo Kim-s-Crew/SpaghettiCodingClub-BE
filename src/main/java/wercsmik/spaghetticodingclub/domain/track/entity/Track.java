2cbb85a50fba0728d44f32e9165c3fb860590f0d
김은채 <ke808762@gmail.com>
2024-05-22 11:15:55 +0900

    feat: Track Entity 생성

--- src/main/java/wercsmik/spaghetticodingclub/domain/track/entity/Track.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/track/entity/Track.java
@@ -1,4 +1,27 @@
 package wercsmik.spaghetticodingclub.domain.track.entity;
 
-public class Track {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import wercsmik.spaghetticodingclub.global.auditing.BaseTimeEntity;
+
+@Entity
+@Getter
+@RequiredArgsConstructor
+public class Track extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String trackName;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
 }
