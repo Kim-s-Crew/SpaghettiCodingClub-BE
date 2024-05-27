7243790edb9321c788ca7e0b06db6cbb1b702476
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-27 20:59:59 +0900

    add: AssessmentService 에서 평가생성 메서드 추가

--- src/main/java/wercsmik/spaghetticodingclub/domain/assessment/service/AssessmentService.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/assessment/service/AssessmentService.java
@@ -1,4 +1,51 @@
 package wercsmik.spaghetticodingclub.domain.assessment.service;
 
+import java.time.LocalDateTime;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import wercsmik.spaghetticodingclub.domain.assessment.dto.AssessmentRequestDTO;
+import wercsmik.spaghetticodingclub.domain.assessment.dto.AssessmentResponseDTO;
+import wercsmik.spaghetticodingclub.domain.assessment.entity.Assessment;
+import wercsmik.spaghetticodingclub.domain.assessment.entity.AssessmentType;
+import wercsmik.spaghetticodingclub.domain.assessment.repository.AssessmentRepository;
+import wercsmik.spaghetticodingclub.domain.user.entity.User;
+import wercsmik.spaghetticodingclub.domain.user.repository.UserRepository;
+import wercsmik.spaghetticodingclub.global.exception.CustomException;
+import wercsmik.spaghetticodingclub.global.exception.ErrorCode;
+
+@Service
+@RequiredArgsConstructor
 public class AssessmentService {
+
+    private final AssessmentRepository assessmentRepository;
+    private final UserRepository userRepository;
+
+    @Transactional
+    public AssessmentResponseDTO createAssessment(User admin, AssessmentRequestDTO assessmentRequestDTO) {
+
+        AssessmentType assessmentType;
+        try {
+            assessmentType = AssessmentType.valueOf(assessmentRequestDTO.getType());
+        } catch (IllegalArgumentException e) {
+            throw new CustomException(ErrorCode.ASSESSMENT_TYPE_NOT_FOUND);
+        }
+
+        // 평가받는 사용자를 조회하는 로직
+        User user = userRepository.findById(assessmentRequestDTO.getUserId())
+                .orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+        Assessment assessment = Assessment.builder()
+                .userId(user) // 평가받는 사용자 설정
+                .adminId(admin)
+                .type(assessmentType)
+                .content(assessmentRequestDTO.getContent())
+                .createdAt(LocalDateTime.now()) // 현재 시간으로 설정
+                .modifiedAt(LocalDateTime.now()) // 현재 시간으로 설정
+                .build();
+
+        Assessment savedAssessment = assessmentRepository.save(assessment);
+
+        return AssessmentResponseDTO.of(savedAssessment);
+    }
 }
