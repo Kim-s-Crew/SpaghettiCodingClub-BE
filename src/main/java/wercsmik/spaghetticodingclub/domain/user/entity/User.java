11dbad6cfe64d35a5b0dc6700d661808c7afc18b
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-27 20:53:46 +0900

    update: Assessment Entity 와 User Entity 간에 관계 추가

--- src/main/java/wercsmik/spaghetticodingclub/domain/user/entity/User.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/user/entity/User.java
@@ -1,17 +1,24 @@
 package wercsmik.spaghetticodingclub.domain.user.entity;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import wercsmik.spaghetticodingclub.domain.assessment.entity.Assessment;
+import wercsmik.spaghetticodingclub.domain.auth.entity.EmailVerification;
 
 @Entity
 @Getter
@@ -38,6 +45,12 @@ public class User {
     @Column(nullable = false)
     @Enumerated(EnumType.STRING)
     private UserRoleEnum role;
+
+    @OneToMany(mappedBy = "userId", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Assessment> receivedAssessments = new ArrayList<>();
+
+    @OneToMany(mappedBy = "adminId", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
+    private final List<Assessment> givenAssessments = new ArrayList<>();
 
     @Builder
     private User(String username, String password, String email, String recommendEmail, UserRoleEnum role) {
