6b68a6665bfd8f4981dc8e789117d48fd88ee9f6
“adashkdy9960 <vjftmxmwlwhs@gmail.com>
2024-05-27 20:56:28 +0900

    add: AssessmentController 평가 생성 기능 추가

--- src/main/java/wercsmik/spaghetticodingclub/domain/assessment/controller/AssessmentController.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/assessment/controller/AssessmentController.java
@@ -1,4 +1,37 @@
 package wercsmik.spaghetticodingclub.domain.assessment.controller;
 
-public class AssessmentController {
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import wercsmik.spaghetticodingclub.domain.assessment.dto.AssessmentRequestDTO;
+import wercsmik.spaghetticodingclub.domain.assessment.dto.AssessmentResponseDTO;
+import wercsmik.spaghetticodingclub.domain.assessment.service.AssessmentService;
+import wercsmik.spaghetticodingclub.global.common.CommonResponse;
+import wercsmik.spaghetticodingclub.global.security.UserDetailsImpl;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/assessmentItems")
+public class AssessmentController{
+
+    private final AssessmentService assessmentService;
+
+    @PostMapping
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    public ResponseEntity<CommonResponse<AssessmentResponseDTO>> createAssessment(
+            @AuthenticationPrincipal UserDetailsImpl userDetails,
+            @RequestBody AssessmentRequestDTO assessmentRequestDTO) {
+
+        AssessmentResponseDTO createdAssessment = assessmentService.createAssessment(userDetails.getUser(),
+                assessmentRequestDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(CommonResponse.of("평가 생성 성공", createdAssessment));
+    }
 }
