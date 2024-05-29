8bd7f2c075debdf34d10e6b7d495beb98be11195
김은채 <ke808762@gmail.com>
2024-05-29 09:59:53 +0900

    feat: null 체크 추가

--- src/main/java/wercsmik/spaghetticodingclub/domain/track/dto/TrackNoticeCreationRequestDTO.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/track/dto/TrackNoticeCreationRequestDTO.java
@@ -1,5 +1,6 @@
 package wercsmik.spaghetticodingclub.domain.track.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -7,7 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public class TrackNoticeCreationRequestDTO {
 
+    @NotBlank(message = "트랙 공지 제목은 필수입니다.")
     private String trackNoticeTitle;
 
+    @NotBlank(message = "트랙 공지 내용은 필수입니다.")
     private String trackNoticeContent;
 }
