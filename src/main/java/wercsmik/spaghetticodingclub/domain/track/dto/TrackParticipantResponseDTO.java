29c04e0ad7de4f6b5745552c57e37c7738e04ae7
김은채 <ke808762@gmail.com>
2024-06-30 03:02:27 +0900

    refactor: 팀참여자id 리팩터링

--- src/main/java/wercsmik/spaghetticodingclub/domain/track/dto/TrackParticipantResponseDTO.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/track/dto/TrackParticipantResponseDTO.java
@@ -1,12 +1,14 @@
 package wercsmik.spaghetticodingclub.domain.track.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @AllArgsConstructor
+@Builder
 public class TrackParticipantResponseDTO {
 
     private Long userId;
--- src/main/java/wercsmik/spaghetticodingclub/domain/track/repository/TrackParticipantsRepository.java
+++ src/main/java/wercsmik/spaghetticodingclub/domain/track/repository/TrackParticipantsRepository.java
@@ -1,16 +1,16 @@
 package wercsmik.spaghetticodingclub.domain.track.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import wercsmik.spaghetticodingclub.domain.track.entity.TrackParticipantId;
 import wercsmik.spaghetticodingclub.domain.track.entity.TrackParticipants;
 
 import java.util.List;
+import java.util.Optional;
 
-public interface TrackParticipantsRepository extends JpaRepository<TrackParticipants, TrackParticipantId> {
+public interface TrackParticipantsRepository extends JpaRepository<TrackParticipants, Long> {
 
     List<TrackParticipants> findByTrackTrackId(Long trackId);
 
     List<TrackParticipants> findByUserUserId(Long userId);
 
-    boolean existsById_UserIdAndId_TrackId(Long userId, Long trackId);
+    Optional<TrackParticipants> findByUser_UserIdAndTrack_TrackId(Long userId, Long trackId);
 }
